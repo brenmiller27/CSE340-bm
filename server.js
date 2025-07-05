02c1843fc89f3612eb0d7a77ec6b3df70c9d6826
brenmiller27 <167664968+brenmiller27@users.noreply.github.com>
2025-07-05 17:25:16 -0600

    initial commit

--- server.js
+++ server.js
@@ -5,11 +5,20 @@
 /* ***********************
  * Require Statements
  *************************/
-const express = require("express")
-const env = require("dotenv").config()
-const app = express()
-const static = require("./routes/static")
-const expressLayouts = require("express-ejs-layouts")
+const express = require("express");
+const env = require("dotenv").config();
+const app = express();
+const static = require("./routes/static");
+const expressLayouts = require("express-ejs-layouts");
+
+/* ***********************
+ * View Engine and Templates
+ *************************/
+
+app.set("view engine", "ejs");
+app.use(expressLayouts);
+app.set("layout", "layouts/layout")
+
 
 /* ***********************
  * Routes
@@ -26,14 +35,17 @@ const host = process.env.HOST
 /* ***********************
  * Log statement to confirm server operation
  *************************/
+
+app.use(express.static('public'));
+
+app.get("/", function(req, res) {
+    res.render("index")
+});
+
+app.use(require ("./routes/static"))
+
 app.listen(port, () => {
   console.log(`app listening on ${host}:${port}`)
 })
 
-app.set("view engine", "ejs");
-app.use(expressLayouts);
-app.set("layout", "./layouts/layout")
 
-app.get("/" , (req, res) => {
-    res.render("index.ejs", {title: Home})
-});
