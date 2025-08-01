a8a062a10c6c64051a6acbbb7bee61ff78aefdd6
brenmiller27 <167664968+brenmiller27@users.noreply.github.com>
2025-07-31 21:34:37 -0600

    initial commit

--- routes/inventoryRoute.js
+++ routes/inventoryRoute.js
@@ -3,6 +3,10 @@ const express = require("express")
 const router = new express.Router() 
 const invController = require("../controllers/invController")
 
+
+const Util = require('../utilities/')
+const handleErrors = require('../utilities')
+
 // Route to build inventory by classification view
 router.get("/type/:classificationId", invController.buildByClassificationId);
 
--- utilities/index.js
+++ utilities/index.js
@@ -1,129 +1,252 @@
+// This file will hold functions that are "utility" in nature, meaning that we will reuse them over and over, but they don't directly belong to the M-V-C structure.
+const utilties = require(".")
+const { body, validationResult } = require("express-validator")
+const jwt = require("jsonwebtoken")
+require("dotenv").config()
+
 const invModel = require("../models/inventory-model")
+const mainModel = require("../models/maintenance-model")
+
+
 const Util = {}
-
-
-/* ************************
- * Constructs the nav HTML unordered list
- ************************** */
-
-Util.getNav = async function( req, res, next) {
-    let data = await invModel.getClassifications()
-    let list = "<ul>"
-    list += '<li><a href="/" title= "Home Page"> </a></li>'
-    data.rows.forEach((row) => {
-        list += "<li>"
-        list +=
-        'a href = "/inv/type' +
-        row.classification_id +
-        ' "title = See Our inventory"' +
-        row.classfication_name +
-        'vehicles">' +
-        row.classfication_name
-        "</a>"
-        list += "</li>"
-        return list
-    })
-}
-
-module.exports = Util
+const validate = {}
 
 
 /* **************************************
-* Build the classification view HTML
-* ************************************ */
+ *  Navigation Menu
+ * ************************************ */
+// Purpose: builds the navigation menu based on car classifications
+Util.getNavigation = async function (req, res, next) {
+    // Block: navigation
+    let data = await invModel.retrieveCarClassifications();
+    let list = "<ul class='navigation__list'>";
 
-Util.buildClassificationGrid = async function(data) {
-    let grid
-    if(data.length > 0) {
-        grid = '<ul id="inv-display">'
-        data.forEach(vehicle => {
-            grid += '<li>'
-            grid +='a href="../../inv/detail/'+ vehicle.inv_id
-            + '"title="view' + vehicle.inv_make + ' ' + vehicle.inv_model
-            + ' on CSE Motors" /></a>'
-        grid += '<div class="namePrice">'
-        grid += '<hr />'
-        grid += '<h2>'
-        grid += '<a href="../../inv/detail/' + vehicle.inv_id +'" title="View ' 
-        + vehicle.inv_make + ' ' + vehicle.inv_model + ' details">' 
-        + vehicle.inv_make + ' ' + vehicle.inv_model + '</a>'
-        grid += '</h2>'
-        grid += '<span>$' 
-        + new Intl.NumberFormat('en-US').format(vehicle.inv_price) + '</span>'
-        grid += '</div>'
-        grid += '</li>'
-        })
-        grid += '</ul>'
-    } else { 
-        grid += '<p class="notice">Sorry, no matching vehicles could be found.</p>'
-    }
-    return grid
-}
+    list += '<li><a href="/" title="Home page" class="navigation__item">Home</a></li>';
+    
+    // Element within Block: navigation__list
+    data.rows.forEach((row) => {
+        list += "<li>";
+        
+        // Element within Block: navigation__item
+        list +=
+            '<a href="/inv/type/' +
+            row.classification_id +
+            '" title="See our inventory of ' +
+            row.classification_name +
+            ' vehicles" class="navigation__item">' +
+            row.classification_name +
+            "</a>";
+        
+        list += "</li>";
+    });
 
-Util.buildItemListing = async function (data) {
-    let listingHtml = "";
-    console.dir({data});
-    if (data) {
-        listingHtml = `
-        <section class="car-listing">
-            <img src= "${data.inv_image}" alt=""
-            <div class = "car-info">
-                <div>
-                <h2?${data.inv_year} ${data.inv_make} </h2>
-                </div>
-            </div>
-            <div>
-                ${Number.parseFloat(data.inv_price).toLocaleString("en-US", {
-                style: "currency",
-                currency: "USD}",
-                })}
-            </div>
-            <div class="description">
-                <p>
-                ${data.inv_description}
-                </p>
-                <dl>
-                    <dt>Mileage</dt>
-                    <dd>${data.inv_miles.toLocaleString("en-US", {
-                    style: "decimal",
-                })}</dd>
-                 <dt>Color</dt>
-                <dd>${data.inv_color}</dd>
-                <dt>Class</dt>
-                <dd>${data.classfication_name}</dd>
-                </dl>
-            </div>
-        </section>
-        `;
-    } else {
-        listingHtml = `
-        <p> Sorry, no matching vehicle data </p>`;
-    }
-    return listingHtml;
+    list += "</ul>";
+    return list;
 };
 
-Util.buildClassificationList = async function (classification_id = null) {
-    let data = await invModel.getClassifcations();
-    let classificationList = 
-    '<select name="classification_id" id="classlist" required>';
-    classificationList += '<option value="' + row.classification_id + '"';
-    if (
-        classification_id !=null &&
-        row.classification_id == classification_id
-    )
-    {
-        classificationList += " selected ";
+
+// **************************************
+// Vehicle List View
+// **************************************
+// Purpose: builds a list of vehicles based on the provided data array.
+Util.buildVehiclesListView = async function (data) {
+  let grid;
+
+  if (data.length > 0) {
+    // Element within Block: vehicle-grid__ul-style
+    grid = '<ul class="vehicle-grid__ul">';
+
+    data.forEach(vehicle => {
+      // Element within Block: vehicle-grid__li
+      grid += '<li class="vehicle-grid__item">'; // Start of li
+      grid += '<div class="vehicle-grid-card grid-cards">'; // start of vehicle grid div inv-display__image-info-container
+
+      // Element within Block: vehicle-grid-card__image-container
+      grid += '<div class="vehicle-grid-card__image-container">'; // start of vehicle grid image-container div
+      grid += '<a href="../../inv/detail/' + vehicle.inv_id +
+        '" title="View ' + vehicle.inv_make + ' ' + vehicle.inv_model +
+        ' details" class="vehicle-grid-card__image-link"><img src="' + vehicle.inv_thumbnail +
+        '" alt="Image of ' + vehicle.inv_make + ' ' + vehicle.inv_model +
+        ' on CSE Motors" class="vehicle-grid-card__image"/></a>';
+      grid += '</div>'; // End of vehicle grid image-container div
+
+      // Element within Block: vehicle-grid-card__text
+      grid += '<div class="vehicle-grid-card__text">'; // start of namePrice div
+
+      grid += '<h1 class="vehicle-grid-card__title">';
+      grid += '<a href="../../inv/detail/' + vehicle.inv_id + '" title="View ' +
+        vehicle.inv_make + ' ' + vehicle.inv_model + ' details" class="vehicle-grid-card__link">' +
+        vehicle.inv_make + ' ' + vehicle.inv_model + ' </a>';
+      grid += '</h1>';
+
+      grid += '<span class="vehicle-grid-card__price">$' + new Intl.NumberFormat('en-US').format(vehicle.inv_price) + '</span>';
+      grid += '</div>'; // End of vehicle grid text div
+
+      grid += '</div>'; // End of vehicle grid container div
+
+      grid += '</li>'; // End of li
+    });
+    grid += '</ul>';
+  } else {
+    // Element within Block: vehicle-grid__li
+    grid += '<li class="vehicle-grid__item">';
+    // Element within Block: notice
+    grid += '<p class="vehicle-grid__notice">Sorry, no matching vehicles could be found.</p>';
+    grid += '</li>';
+  }
+
+  // returns the variable to the calling location.
+  return grid;
+};
+
+
+/* **************************************
+ * Vehicle Detail View
+ * ************************************ */
+// Prupose: builds the vehicle detail view  
+Util.buildVehicleDetailview = async function(data) {
+    let grid = '';
+
+    if (data.length > 0) {
+        data.forEach(vehicle => {
+            // Block: vehicle-detail-card
+            grid += '<div class="vehicle-detail-card__container">'; // Start of container div
+
+            // Element within Block: vehicle-detail-card__title
+            //grid += `<div class="vehicle-detail-card__title"><h1>${vehicle.inv_make} ${vehicle.inv_model}</h1></div>`; // title div
+
+            // Element within Block: vehicle-detail-card__content
+            grid += `<div class="vehicle-detail-card__content">` // Start of vehicle detail content div
+
+            // Element within Block: vehicle-detail-card__image
+            grid += `<div class="vehicle-detail-card__image-container">` // Start of vehicle detail image div
+            grid += `<img src=" ${vehicle.inv_image}" alt="${vehicle.inv_make} ${vehicle.inv_model}" class="vehicle-detail-card__image">`;
+            grid += '</div>'; // End of vehicle detail image div
+
+            // Element within Block: vehicle-detail-card__text
+            grid += `<div class="vehicle-detail-card__text">` // Start of vehicle detail text div
+            grid += `<p>Availability: ${vehicle.vehicle_status_type}</p>`;
+            grid += `<p>Year: ${vehicle.inv_year}</p>`;
+            grid += `<p>Price: $${new Intl.NumberFormat('en-US').format(vehicle.inv_price)}</p>`;
+            grid += `<p>Mileage: ${new Intl.NumberFormat('en-US').format(vehicle.inv_miles)}</p>`;
+            grid += `</div>`; // End of vehicle detail text div
+
+            grid += `</div>`; // End of vehicle detail content div
+
+            grid += '</div>'; // End of vehicle detail container div
+        });
+    } else {
+        // Element within Block: notice
+        grid += '<p class="notice">Sorry, no matching vehicles could be found.</p>';
     }
 
-    classificationList += ">" + row.classfication_name
+    return grid;
+};
+
+
+/* **************************************
+ * Classification Select List
+ * ************************************ */
+// Purpose: builds the select list for car classifications.
+Util.buildVehicleClassificationSelectList = async function (classification_id = null) {
+    let data = await invModel.retrieveCarClassifications();
+    let classificationList =
+      '<select name="classification_id" id="classificationList" >';
+    classificationList += "<option>Choose a Classification</option>";
+    data.rows.forEach((row) => {
+      classificationList += `<option value="${row.classification_id}"${
+        classification_id != null && row.classification_id == classification_id
+          ? " selected"
+          : ""
+      }>${row.classification_name}</option>`;
+      if (
+        classification_id != null &&
+        row.classification_id == classification_id
+      ) {
+        classificationList += " selected ";
+      }
+      classificationList += `>${row.classification_name}</option>}`;
+    });
+    classificationList += "</select>";
+    
+    return classificationList;
+  };
+
+
+/* **************************************
+* Inventory/Classification Managment Tool
+* ************************************ */
+// Purpose: builds the Inventory Management Buttons -- add inventory and add classification.
+Util.buildInventoryManagementButtons = async function () {
+    let managementButtons = `
+    <button type="button" class="management__button"><a href="/inv/add-classification" class="management-button-link">Add New Classification</a></button>
+    <button type="button" class="management__button"><a href="/inv/add-inventory" class="management-button-link">Add New Vehicle</a></button>
+    `;
+    return managementButtons;
+  };
+
+/* ****************************************
+ *  Check Login
+ * ************************************ */
+// Purpose: check if logged in
+Util.checkLogin = (req, res, next) => {
+  if (res.locals.loggedIn) {
+    next()
+  } else {
+    req.flash("notice", "Please log in.")
+    return res.redirect("/account/login")
+  }
 }
+
+/* ****************************************
+ *  Check Account Type
+ * ************************************ */
+// Purpose: check account type
+Util.checkAccountType = (req, res, next) => {
+  if (res.locals.loggedIn) {
+    if (res.locals.accountData.account_type == "Admin" || res.locals.accountData.account_type == "Employee") {
+    next()
+    }
+  } else {
+    return res.redirect("/account/login")
+  }
+}
+
+/* ------------------------------------------------------------------------------------------------------------------- */
+
+
+
+
+/* ------------------------------------------------------------------------------------------------------------------- */
 
 /* ****************************************
  * Middleware For Handling Errors
  * Wrap other function in this for 
  * General Error Handling
  **************************************** */
+Util.handleErrors = fn => (req, res, next) => Promise.resolve(fn(req, res, next)).catch(next)
 
-Util.handleErrors = fn => (req, res , next) => Promise.resolve(fn(req,res, next)).catch(next)
+/* ****************************************
+* Middleware to check token validity
+**************************************** */
+Util.checkJWTToken = (req, res, next) => {
+    if (req.cookies.jwt) {
+     jwt.verify(
+      req.cookies.jwt,
+      process.env.ACCESS_TOKEN_SECRET,
+      function (err, accountData) {
+       if (err) {
+        req.flash("Please log in")
+        res.clearCookie("jwt")
+        return res.redirect("/account/login")
+       }
+       res.locals.accountData = accountData
+       res.locals.loggedIn = 1
+       next()
+      })
+    } else {
+     next()
+    }
+   }
 
 module.exports = Util;
